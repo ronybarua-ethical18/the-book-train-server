aa7457c774b2b7a497ccf4f5e45c179574f83d25
ronybarua.ethical18 <ronybarua.ethical18@gmail.com>
2021-07-01 01:07:50 +0600

    backend api fixed

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ client.connect(err => {
   // read or retrieve data from database 
   app.get('/books', (req, res) => {
     const search = req.query.search
-    booksCollection.find({name: {$regex: search}})
+    booksCollection.find({} ||{ name: {$regex: search}})
       .toArray((error, documents) => {
         res.send(documents);
       })
